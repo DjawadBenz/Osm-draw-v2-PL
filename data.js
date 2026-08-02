504f6d1fa3d97541e47cefba94ecdad0c1f43a4e
Djawad Benzira <82314428+DjawadBenz@users.noreply.github.com>
2026-08-02 21:16:30 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+// data.js
+// You can easily add, remove, or change teams in this file.
+const initialTeams = [
+    { name: "Arsenal", flag: "arsenal.football-logos.cc.png" },
+    { name: "Aston Villa", flag: "aston-villa.football-logos.cc.png" },
+    { name: "Bournemouth", flag: "bournemouth.football-logos.cc.png" },
+    { name: "Chelsea", flag: "chelsea.football-logos.cc.png" },
+    { name: "Liverpool", flag: "liverpool.football-logos.cc.png" },
+    { name: "Manchester City", flag: "manchester-city.football-logos.cc.png" },
+    { name: "Manchester United", flag: "manchester-united.football-logos.cc.png" },
+    { name: "Newcastle", flag: "newcastle.football-logos.cc.png" },
+    { name: "Tottenham", flag: "tottenham.football-logos.cc.png" }
+];
